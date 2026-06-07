f3a4badacaf9210ff053786dba517926b85be600
Harshitha D <harshitha7d@gmail.com>
2026-06-07 15:58:32 +0530

    added api key to config

--- src/config/config.js
+++ src/config/config.js
@@ -28,7 +28,8 @@ const  config= {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
-  GOOGLE_USER: process.env.GOOGLE_USER
+  GOOGLE_USER: process.env.GOOGLE_USER,
+  RESEND_API_KEY:process.env.RESEND_API_KEY
 }
 
 export default config;
