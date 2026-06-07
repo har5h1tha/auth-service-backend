140f3e7636470a66fac2a0d0a558de140e31b5de
Harshitha D <harshitha7d@gmail.com>
2026-06-07 15:55:41 +0530

    changed from OAuth to RESEND

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,40 +1,22 @@
-import nodemailer from 'nodemailer';
-import config from '../config/config.js';
+import { Resend } from "resend";
+import config from "../config/config.js";
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
-        user: config.GOOGLE_USER,
-        clientId: config.GOOGLE_CLIENT_ID,
-        clientSecret: config.GOOGLE_CLIENT_SECRET,
-        refreshToken: config.GOOGLE_REFRESH_TOKEN
-    }
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.error('Error connecting to email server:', error);
-  } else {
-    console.log('Email server is ready to send messages');
-  }
-});
-
+const resend = new Resend(config.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, text, html) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Your Name" <${config.GOOGLE_USER}>`, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      html, // html body
+    const response = await resend.emails.send({
+      from: "Auth System <onboarding@resend.dev>",
+      to,
+      subject,
+      text,
+      html,
     });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log("Email sent:", response);
+    return response;
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Email error:", error);
+    throw error;
   }
 };
-export default sendEmail;
