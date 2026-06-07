d14eed4cdb3722063a4713769e86dce82996131d
Harshitha D <harshitha7d@gmail.com>
2026-06-07 15:56:44 +0530

    syntax error in rateLimiter

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit'
 
 export const authLimiter=rateLimit({
-    windowMs=15*60*1000,
+    windowMs:15*60*1000,
     max:5,
     message:{
         message:"TOO many requests try ,again later"
